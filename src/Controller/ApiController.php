205dad086dfbecc518b496a2d4616f1f7e8964ef
Noah Greff <greffnoah@gmail.com>
2020-11-17 19:29:57 +0100

    adding details/error for err response

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -84,7 +84,7 @@ class ApiController
             if ($response->getStatusCode() != 201)
             {
                 if (isset($jsonData['error']) && $jsonData['error'] == 'User already exists.') $this->errorMessage = 'Email already taken.'; 
-                else $this->errorMessage = $jsonData['error'];
+                else $this->errorMessage = [ $jsonData['details'], [ 'details' => $jsonData['details'] ]];
             }
             else
             {
